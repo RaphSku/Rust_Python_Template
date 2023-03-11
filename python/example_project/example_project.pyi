650f283d1c51c3c976835e82e85e1082d615ee89
RaphSku <rapsku.dev@gmail.com>
2023-03-11 11:29:44 +0100

    Type hints + docs for binding

---
+++ python/example_project/example_project.pyi
@@ -0,0 +1,32 @@
+def parallelized_sum_values_in_array(array: list[int]) -> int:
+    """
+    Parallelizing a sum over an array
+
+    Args
+    ----
+    array: list[int]
+        Input array which should be summed over
+
+    Returns
+    -------
+    int
+        The result of the summation
+    """
+    ...
+
+
+def sum_values_in_array(array: list[int]) -> int:
+    """
+    Summation over an array
+
+    Args
+    ----
+    array: list[int]
+        Input array
+
+    Returns
+    -------
+    int
+        Result of summation
+    """
+    ...
